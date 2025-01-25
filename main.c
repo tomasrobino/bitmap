cde955b8074baad1218b247f66dc0090945a79ee
Tomas <tomaserobino@gmail.com>
2025-01-25 21:25:12 +0100

    switched dword to integers

--- main.c
+++ main.c
@@ -85,20 +85,20 @@ void readBMP(char name[]) {
     //All headers have a size=dibSize except for when dibSize=16
     //Setting types
     //offset 14
-    DWORD dibSize;
-    fread(&dibSize, sizeof(DWORD), 1, file);
+    unsigned int dibSize;
+    fread(&dibSize, sizeof(unsigned int), 1, file);
 
     //FROM THIS POINT ON, ONLY BITMAPINFOHEADER AND SUCCESSIVE VERSIONS ARE IMPLEMENTED
 
     //Width in pixels
     //offset 18
-    DWORD width;
-    fread(&width, sizeof(DWORD), 1, file);
+    int width;
+    fread(&width, sizeof(int), 1, file);
 
     //Height in pixels
     //offset 22
-    DWORD height;
-    fread(&height, sizeof(DWORD), 1, file);
+    int height;
+    fread(&height, sizeof(int), 1, file);
 
     //Number of color planes
     //Must be 1 if header specifically is BITMAPINFOHEADER
@@ -113,55 +113,55 @@ void readBMP(char name[]) {
 
     //Compression method being used
     //offset 30
-    DWORD compression;
-    fread(&compression, sizeof(DWORD), 1, file);
+    unsigned int compression;
+    fread(&compression, sizeof(unsigned int), 1, file);
 
     //Size of the raw bitmap data; a dummy 0 can be given for BI_RGB bitmaps.
     //offset 34
-    DWORD dataSize;
-    fread(&dataSize, sizeof(DWORD), 1, file);
+    unsigned int dataSize;
+    fread(&dataSize, sizeof(unsigned int), 1, file);
 
     //Horizontal resolution of the image. (pixel per metre, signed integer)
     //offset 38
-    DWORD horizontalRes;
-    fread(&horizontalRes, sizeof(DWORD), 1, file);
+    int horizontalRes;
+    fread(&horizontalRes, sizeof(int), 1, file);
 
     //Vertical resolution of the image. (pixel per metre, signed integer)
     //offset 42
-    DWORD verticalRes;
-    fread(&verticalRes, sizeof(DWORD), 1, file);
+    int verticalRes;
+    fread(&verticalRes, sizeof(int), 1, file);
 
     //The number of colors in the color palette, or 0 to default to 2^n
     //offset 46
-    DWORD colorNum;
-    fread(&colorNum, sizeof(DWORD), 1, file);
+    unsigned int colorNum;
+    fread(&colorNum, sizeof(unsigned int), 1, file);
 
     //The number of important colors used, or 0 when every color is important; generally ignored
     //offset 50
-    DWORD importantColors;
-    fread(&importantColors, sizeof(DWORD), 1, file);
+    unsigned int importantColors;
+    fread(&importantColors, sizeof(unsigned int), 1, file);
 
     //ONLY FOR BITMAPV4HEADER
     if (size==108 || size==124) {
         //red, green and blue Masks, specifies the color component of each pixel,
         //valid only if compression is set to BI_BITFIELDS.
         //offset 54
-        DWORD redMask;
-        fread(&redMask, sizeof(DWORD), 1, file);
+        unsigned int redMask;
+        fread(&redMask, sizeof(unsigned int), 1, file);
         //offset 58
-        DWORD greenMask;
-        fread(&greenMask, sizeof(DWORD), 1, file);
+        unsigned int greenMask;
+        fread(&greenMask, sizeof(unsigned int), 1, file);
         //offset 62
-        DWORD blueMask;
-        fread(&blueMask, sizeof(DWORD), 1, file);
+        unsigned int blueMask;
+        fread(&blueMask, sizeof(unsigned int), 1, file);
         //Color mask that specifies the alpha component of each pixel
         //offset 66
-        DWORD alphaMask;
-        fread(&alphaMask, sizeof(DWORD), 1, file);
+        unsigned int alphaMask;
+        fread(&alphaMask, sizeof(unsigned int), 1, file);
         //The color space of the DIB. Only possible value is LCS_CALIBRATED_RGB
         //offset 70
-        DWORD colorSpace;
-        fread(&colorSpace, sizeof(DWORD), 1, file);
+        unsigned int colorSpace;
+        fread(&colorSpace, sizeof(unsigned int), 1, file);
         //A CIEXYZTRIPLE structure that specifies the x, y, and z coordinates
         //of the three colors that correspond to the red, green, and blue endpoints for the logical
         //color space associated with the bitmap.
@@ -173,30 +173,30 @@ void readBMP(char name[]) {
         //These members are ignored unless color values are calibrated RGB values
         //and colorSpace is set to LCS_CALIBRATED_RGB.
         //offset 110
-        DWORD redGamma;
-        fread(&redGamma, sizeof(DWORD), 1, file);
+        unsigned int redGamma;
+        fread(&redGamma, sizeof(unsigned int), 1, file);
         //offset 114
-        DWORD greenGamma;
-        fread(&greenGamma, sizeof(DWORD), 1, file);
+        unsigned int greenGamma;
+        fread(&greenGamma, sizeof(unsigned int), 1, file);
         //offset 118
-        DWORD blueGamma;
-        fread(&blueGamma, sizeof(DWORD), 1, file);
+        unsigned int blueGamma;
+        fread(&blueGamma, sizeof(unsigned int), 1, file);
 
     }
 
     if (size==124) {
         //Rendering intent for bitmap
         //offset 122
-        DWORD intent;
-        fread(&intent, sizeof(DWORD), 1, file);
+        unsigned int intent;
+        fread(&intent, sizeof(unsigned int), 1, file);
         //The offset, in bytes, from the beginning of the BITMAPV5HEADER structure to the start of the profile data
         //offset 126
-        DWORD profileData;
-        fread(&profileData, sizeof(DWORD), 1, file);
+        unsigned int profileData;
+        fread(&profileData, sizeof(unsigned int), 1, file);
         //Size, in bytes, of embedded profile data
         //offset 130
-        DWORD profileSize;
-        fread(&profileSize, sizeof(DWORD), 1, file);
+        unsigned int profileSize;
+        fread(&profileSize, sizeof(unsigned int), 1, file);
         //Skipping reserved bytes
         //offset 134
         fseek(file, 4, SEEK_CUR);
